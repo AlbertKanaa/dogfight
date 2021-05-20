9c8ba8a05f3a5a449900627fb8ea3b7c037a3050
kanaa <kanaa@DESKTOP-6UALB86>
2021-05-20 19:47:23 +0100

    phase 2

--- src/jpu2016/dogfight/controller/Iorderperformer.java
+++ src/jpu2016/dogfight/controller/Iorderperformer.java
@@ -2,4 +2,7 @@ package jpu2016.dogfight.controller;
 
 public interface Iorderperformer {
 
+	public void orderperform(Userorder userorder) {
+		
+	}
 }
--- src/jpu2016/dogfight/controller/Userorder.java
+++ src/jpu2016/dogfight/controller/Userorder.java
@@ -1,5 +1,15 @@
 package jpu2016.dogfight.controller;
 
-public class Userorder {
-
+public class Userorder implements Iuserorder {
+	private int player;
+	
+	public Userorder(int player, Order order) {
+		
+	}
+	public Order getOrder() {
+		return getOrder();
+	}
+	public int getPlayer() {
+		return player;
+	}
 }
